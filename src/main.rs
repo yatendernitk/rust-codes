4c7c6f60218115783836a0efed89c5d0c3dd5b7a
ok <ok@Yatenders-MacBook-Pro-2.local>
2017-10-18 18:14:59 +0530

    compare game completed

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,22 @@ fn main() {
   guessing();
 }
 
-fn guessing(){
+fn guessing() {
   let secret_number = rand::thread_rng().gen_range(1, 101);
   println!("Guess the number");
 
-  println!("secret number is {}", secret_number);
+  // println!("secret number is {}", secret_number);
 
   loop {
     println!("Enter your number");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
     .expect("Failed to read Line");
-
     println!("You guessed the number {}", guess);
-
-    let mut guess: u32 = guess.trim().parse()
-      .expect("failed to convert string to interger");
+    let mut guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
 
     match guess.cmp(&secret_number) {
         Ordering::Less    => println!("Too small.. "),
